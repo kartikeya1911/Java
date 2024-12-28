fd8fe7886ed290206087a54ccf805dba94da92c7
kartikeya1911 <jainkartikeya9@gmail.com>
2024-12-28 14:18:41 +0530

    java programs

---
+++ PrintHello.java
@@ -0,0 +1,19 @@
+public class PrintHello {
+
+    public static void main(String[] args) {
+        // System.out.println("Hello World");
+        // System.out.println("bye to you");
+
+        // System.out.println((3 + 5 - 3) * 2);
+
+        // int num1 = 21;
+        // int num2 = 12;
+        // int result=num1+num2;
+        // System.out.println(result);
+
+        int a=3;
+        double b=5.4;
+        double c=a+b;
+        System.out.println(c);
+    }
+}
---
+++ TypeConversion.java
@@ -0,0 +1,24 @@
+public class TypeConversion {
+    public static void main(String[] args){
+// int to double:-
+
+        // int a=13;
+        // double d=(double)a;
+
+//double to int:-
+
+        // double a = 15.63;
+        // int b=(int)a;
+
+//int to char:-
+
+        // int a=65;
+        // char c=(char)a;
+
+//char to int:-
+
+        char c = 'A';
+        int a=(int)c ;
+        System.out.println(a);
+    }
+}
