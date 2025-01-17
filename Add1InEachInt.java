8ee41107693a48beb032e3f52704e9f209155621
kartikeya1911 <jainkartikeya9@gmail.com>
2025-01-17 23:33:56 +0530

    sample try

--- Add1InEachInt.java
+++ Add1InEachInt.java
@@ -1,2 +1,18 @@
 //input 1234
 //output 2345
+public class Add1InEachint{
+    public static void main(String[] args) {
+        int n = 1234;
+        int sum = 0;
+        while(n>0){
+            int rem = n%10;
+            sum = sum + rem;
+            n = n/10;
+            }
+            return sum;
+            }
+            public static void main(String[] args) {
+                int n = 12345;
+                System.out.println(sumOfDigits(n));
+                }
+}
